16a62bfd17ac752af8e4f335d440f3f410d3551e
Alex Clark <aclark@aclark.net>
2012-08-02 11:05:43 -0400

    Get all the repos

--- org_repos_cloner/__init__.py
+++ org_repos_cloner/__init__.py
@@ -1,5 +1,14 @@
+import json
 import requests
 
 
+GITHUB = 'https://api.github.com'
+ORG = 'plone'
+NUM_PAGES = range(3)  # XXX Fix me, use API to get the real number of pages
+
+
 def clone_org_repos():
-    print 'Hello world'
+    for page in NUM_PAGES:
+        repos = requests.get('%s/orgs/%s/repos?per_page=100&page=%s' % (GITHUB, ORG, page + 1)).content
+        for repo in json.loads(repos):
+            print repo['git_url']
