3fcc59c49d095304fc2fd6d562df45154b01772c
Alex Clark <aclark@aclark.net>
2012-08-02 11:06:28 -0400

    Flake8 fix

--- org_repos_cloner/__init__.py
+++ org_repos_cloner/__init__.py
@@ -9,6 +9,7 @@ NUM_PAGES = range(3)  # XXX Fix me, use API to get the real number of pages
 
 def clone_org_repos():
     for page in NUM_PAGES:
-        repos = requests.get('%s/orgs/%s/repos?per_page=100&page=%s' % (GITHUB, ORG, page + 1)).content
+        repos = requests.get('%s/orgs/%s/repos?per_page=100&page=%s' % (
+            GITHUB, ORG, page + 1)).content
         for repo in json.loads(repos):
             print repo['git_url']
